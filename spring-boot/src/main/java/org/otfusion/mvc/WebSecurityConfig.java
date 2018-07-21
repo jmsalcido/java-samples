2b510d394dea59d92621e2fa5c042ba279f84bcb
jmsalcido <jose@otfusion.org>
2018-07-21 13:56:56 -0700

    spring boot update
    add Spring Security basic http auth for Actuator + H2Console

--- spring-boot/src/main/java/org/otfusion/mvc/WebSecurityConfig.java
+++ spring-boot/src/main/java/org/otfusion/mvc/WebSecurityConfig.java
@@ -1,25 +1,55 @@
 package org.otfusion.mvc;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/github/**").permitAll();
+
+        http.authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info", "health")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
                 .and()
-                .authorizeRequests().antMatchers("/github/**").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").hasRole("ADMIN")
                 .and()
                 .headers().frameOptions().disable()
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**")
                 .and()
-                .cors().disable();
+                .cors().disable().httpBasic();
+
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("s3cr3t")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
